e032a6b7613e34b32895caa3ea2611f47d5e6028
jlbm <j.luis.baroja@gmail.com>
2017-05-31 17:24:28 -0500

    Daily weights plus date correction of past three days

--- plot_weights.R
+++ plot_weights.R
@@ -5,10 +5,14 @@ days_labs <- names(bw)[2:length(names(bw))]
 # x11(width=5,height=5)
 pdf(file='tracking_weights.pdf',width=7,height=6)
 par(mar=c(8,5,2,2))
-plot(NULL,type='n',xlim=c(1,length(days_labs)),ylim=c(300,700),
+plot(NULL,type='n',xlim=c(1,length(days_labs)),ylim=c(0,700),
      axes=F,ann=F)
 for(r in 1:nrow(bw)){
-  lines(1:length(days_labs),bw[r,days_labs])
+  col <- 'black'
+  if(bw$Bird[r]%in%c('P168','P498','P327','P891')){
+    col <- 'red'
+  }
+  lines(1:length(days_labs),bw[r,days_labs],col=col)
 }
 axis(2)
 axis(1,at=1:length(days_labs),days_labs,las=2,cex.axis=.8)
